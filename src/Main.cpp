5c4f4708f205f1e46c85903dd4a4a07f67ec18a1
Dreyka Oas <dreykaoas@fedora.home>
2026-02-16 15:36:18 +0100

    ci: fix compilation errors and strengthen dependencies for all platforms

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#pragma comment(lib, "external/raylib/lib/raylib.lib")
-#pragma comment(lib, "winmm.lib")
-#pragma comment(lib, "gdi32.lib")
-#pragma comment(lib, "opengl32.lib")
-#pragma comment(lib, "user32.lib")
-#pragma comment(lib, "shell32.lib")
-
-#include "Core/Application.h"
+﻿#include "Core/Application.h"
 
 int main() {
     Application app;
